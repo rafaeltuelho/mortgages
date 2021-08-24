c0cd60b53819b5b2b62d4222b617b2ac20253e5d
Rafael T. C. Soares <rafaelcba@gmail.com>
2021-08-23 20:30:15 -0400

    adding unit tests

---
+++ src/test/java/mortgages/ScenarioJunitActivatorTest.java
@@ -0,0 +1,8 @@
+package mortgages;
+/**
+* Do not remove this file
+*/
+@org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
+// @org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
+public class ScenarioJunitActivatorTest {
+}
